3c17fb585abded5147229951e305d34c2ac9cce7
jean-alexandre iragne <jean-alexandreiragne@jean-alexandres-MacBook-Pro.local>
2015-07-03 18:21:30 +0200

    update

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 'use strict';
 
 
-// var { RNAppInfo } = require('react-native').NativeModules;
+ var { RNAppInfo } = require('react-native').NativeModules;
 
-var AppInfo = function (){
-
+var AppInfo = {
+	getInfoVersion: function (){
+		return RNAppInfo.version;
+	},
+	getInfoShortVersion: function (){
+		return RNAppInfo.shortVersion;
+	},
+	getInfoBundleIdentifier: function (){
+		return RNAppInfo.bundleIdentifier;
+	},
+	getInfoName: function (){
+		return RNAppInfo.name;
+	},
+	getInfoDisplayName: function (){
+		return RNAppInfo.displayName;
+	}
 };
-	// getInfoVersion: function (){
-	// 	return RNAppInfo.version;
-	// },
-	// getInfoShortVersion: function (){
-	// 	return RNAppInfo.shortVersion;
-	// },
-	// getInfoBundleIdentifier: function (){
-	// 	return RNAppInfo.bundleIdentifier;
-	// },
-	// getInfoName: function (){
-	// 	return RNAppInfo.name;
-	// },
-	// getInfoDisplayName: function (){
-	// 	return RNAppInfo.displayName;
-	// }
-// };
-// AppInfo.locale = RNAppInfo.locale;
+
 
 module.exports =  AppInfo;

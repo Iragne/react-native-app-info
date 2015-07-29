6dc5a916ad842712b382d7bc0ed13c1d6c76c517
jean-alexandre iragne <jean-alexandreiragne@jean-alexandres-MacBook-Pro.local>
2015-07-29 11:38:19 +0200

    Add device informations

--- RNAppInfo/RNAppInfo.m
+++ RNAppInfo/RNAppInfo.m
@@ -17,12 +17,19 @@ RCT_EXPORT_MODULE();
     NSString *displayName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
     if(displayName == nil)
         displayName = @"";
+    
+    NSString *iosVersion = [[UIDevice currentDevice] systemVersion];
+    NSString *infoDeviceName = [[UIDevice currentDevice] name];
+    
     return @{
              @"version": [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"],
              @"shortVersion": [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"],
              @"bundleIdentifier": [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleIdentifier"],
              @"name": [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"],
              @"displayName": displayName,
+             @"getInfoiOS":iosVersion,
+             @"getInfoDeviceName":infoDeviceName,
+             
     };
 }
 RCT_EXPORT_METHOD(setNetworkActivityIndicatorVisible:(BOOL)active) {
